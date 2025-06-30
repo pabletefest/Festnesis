eaa4d1de2f9d70d61c7eeaffb918145e41c1c684
pabletefest <stemmerpablo@gmail.com>
2025-06-30 20:31:38 +0200

    Added read/write functions to m68k core

--- include/cpu/m68000.hpp
+++ include/cpu/m68000.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "m68000_regs.hpp"
+#include <concepts>
 
 class M68000 {
 public:
@@ -9,6 +10,12 @@ public:
 
   auto reset() -> void;
   auto runInstruction() -> std::uint8_t;
+
+  template<std::unsigned_integral T>
+  auto read(std::uint32_t address) const -> T;
+
+  template<std::unsigned_integral T>
+  auto write(std::uint32_t address, T) -> void;
 
 private:
   M68000Regs regs{};
--- src/cpu/m68000.cpp
+++ src/cpu/m68000.cpp
@@ -1,5 +1,8 @@
 #include "cpu/m68000.hpp"
 
+#include <bit>
+#include <endian.h>
+
 M68000::M68000() {}
 
 auto M68000::reset() -> void {}
@@ -8,3 +11,20 @@ auto M68000::runInstruction() -> std::uint8_t {
   return std::uint8_t();
 }
 
+template<std::unsigned_integral T>
+auto M68000::read(std::uint32_t address) const -> T {
+  T value = T();
+  
+  if constexpr (std::endian::native == std::endian::little) {
+    return std::byteswap(value);
+  }
+
+  return value;
+}
+
+template<std::unsigned_integral T>
+auto M68000::write(std::uint32_t address, T value) -> void {
+  if constexpr (std::endian::native == std::endian::little) {
+    std::byteswap(value);
+  }
+}
